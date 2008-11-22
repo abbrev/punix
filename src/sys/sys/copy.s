60a55bc289fca88b8b8233954741d04a547ee288
Christopher Williams <abbrev@gmail.com>
2008-11-22 13:00:13 -0700

    added basic memory protection to copyin/copyout routines

--- src/sys/sys/copy.s
+++ src/sys/sys/copy.s
@@ -1,5 +1,5 @@
-/* really simple copyin and copyout functions.
- * these should be implemented to catch invalid memory ranges.
+/* copyin and copyout
+ * these catch obvious invalid memory ranges.
  * since the MC68000 doesn't have an MMU, we can only do so much, as far
  * as memory protection and fault detection go. :(
  */
@@ -9,15 +9,43 @@
 
 .section _st1, "x"
 
-/* int copyout(void *dest, void *src, size_t count) */
-copyout:
+| check %d1 through %d1 + %d0 for valid address-ness
+chkaddr:
+	cmp.l	#0x6000,%d1	| low address XXX: constant
+	blo.s	1f
+	move.l	%d1,%d2
+	add.l	%d0,%d2		| %d2 is end address of range
+	cmp.l	%d2,%d1		| check for wrap-around
+	bhi.s	1f
+	cmp.l	#0x40000,%d2	| high address
+	bhi.s	1f
+	rts	| looks aight...
+1:	| Fault!
+	lea.l	4(%sp),%sp	| pop off our return address
+	move	#14,%d0		| XXX: EFAULT
+	rts
 
 /* int copyin(void *dest, void *src, size_t count) */
 copyin:
-	move.l	4(%sp),%a0	| dest
-	move.l	8(%sp),%a1	| src
 	move.l	12(%sp),%d0	| count
 	beq.b	1f
+	move.l	8(%sp),%d1	| src
+	bsr	chkaddr
+	move.l	%d1,%a1
+	move.l	4(%sp),%a0	| dest
+0:	move.b	(%a1)+,(%a0)+
+	subq.l	#1,%d0
+	bne.s	0b
+1:	rts
+
+/* int copyout(void *dest, void *src, size_t count) */
+copyout:
+	move.l	12(%sp),%d0	| count
+	beq.b	1f
+	move.l	4(%sp),%d1	| dest
+	bsr	chkaddr
+	move.l	%d1,%a0
+	move.l	8(%sp),%a1	| src
 0:	move.b	(%a1)+,(%a0)+
 	subq.l	#1,%d0
 	bne.s	0b
