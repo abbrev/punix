2c0d8aafd0c0a3127f8baf335a54314fa0f0b828
Christopher Williams <abbrev@gmail.com>
2009-03-31 10:00:47 -0700

    added an inverted glyph drawing routine, drawglyphinv

--- src/sys/sys/drawglyph.s
+++ src/sys/sys/drawglyph.s
@@ -24,7 +24,6 @@
 .global drawglyph
 | 6x4 (small) glyph
 | drawglyph(struct glyph *glyph, int row, int col)
-	dc.l	0xbeeff00b
 drawglyph:
 	movem.l	%d3/%d4,-(%sp)
 	move.l	8+4(%sp),%a1	| glyph
@@ -55,7 +54,7 @@ drawglyph:
 	and.b	%d3,%d1		| mask glyph
 	
 	and.b	%d4,(%a0)	| mask byte on screen
-	or.b	%d1,(%a0)	| and put glyph row there
+	eor.b	%d1,(%a0)	| and put glyph row there
 	
 	| FIXME: for greyscale, change the line below to do the same as above,
 	| except write to the light plane instead of the dark plane.
@@ -66,7 +65,60 @@ drawglyph:
 	
 	| %a1 is on light plane
 	and.b	%d4,(%a1)	| mask byte on screen
-	or.b	%d1,(%a1)	| and put glyph row there
+	eor.b	%d1,(%a1)	| and put glyph row there
+	
+	add	%d0,%a1		| next screen row
+	*/
+	
+	add	%d0,%a0		| next screen row
+	dbra	%d2,0b
+	
+	movem.l	(%sp)+,%d3/%d4
+	rts
+
+.global drawglyphinv
+| 6x4 (small) glyph, inverted
+| drawglyphinv(struct glyph *glyph, int row, int col)
+drawglyphinv:
+	movem.l	%d3/%d4,-(%sp)
+	move.l	8+4(%sp),%a1	| glyph
+	move	8+8(%sp),%d1	| row
+	move	8+10(%sp),%d2	| col
+	
+	| get address on screen
+	mulu	#6*LCD_INCY,%d1
+	
+	move.b	#0xf0,%d3	| AND mask for glyph and the OR mask for screen
+	asr	#1,%d2		| col / 2
+	bcc.s	0f		| is col even?
+	moveq	#0x0f,%d3
+0:
+	add	%d2,%d1		| + col
+	move	%d1,%a0
+	lea	LCD_MEM(%a0),%a0	| address on screen
+	| %a0 holds screen address
+	| %d3.b holds glyph mask and screen mask
+	| %a1 holds glyph address
+	
+	moveq	#LCD_INCY,%d0
+	moveq	#6-1,%d2	| glyph height - 1
+0:
+	move.b	(%a1)+,%d1	| get a row from the glyph
+	and.b	%d3,%d1		| mask glyph
+	
+	or.b	%d3,(%a0)	| mask byte on screen
+	eor.b	%d1,(%a0)	| and put glyph row there
+	
+	| FIXME: for greyscale, change the line below to do the same as above,
+	| except write to the light plane instead of the dark plane.
+	addq.l	#1,%a1
+	/*
+	move.b	(%a1)+,%d1	| get a row of glyph
+	and.b	%d3,%d1		| mask glyph
+	
+	| %a1 is on light plane
+	or.b	%d3,(%a1)	| mask byte on screen
+	eor.b	%d1,(%a1)	| and put glyph row there
 	
 	add	%d0,%a1		| next screen row
 	*/
