4739ddadf2323254f12cf745241c95ccdf1d3301
Christopher Williams <abbrev@gmail.com>
2009-04-08 09:44:49 -0700

    need struct utsname too

--- include/sys/utsname.h
+++ include/sys/utsname.h
@@ -1,4 +1,14 @@
 #ifndef _SYS_UTSNAME_H_
 #define _SYS_UTSNAME_H_
 
+struct utsname {
+	char sysname[9];
+	char nodename[9];
+	char release[9];
+	char version[33];
+	char machine[9];
+};
+
+int uname(struct utsname *);
+
 #endif
