3980b910c4d06b26a8efdfa9a3ec17519b77f404
Christopher Williams <abbrev@gmail.com>
2009-11-07 15:41:13 -0700

    added cpuidle() routine that actually turns off the cpu

--- src/sys/sys/m68k.s
+++ src/sys/sys/m68k.s
@@ -32,11 +32,27 @@ defspl	7 | block everything (except memory write error)
 .global nop
 nop:	rts
 
-| void halt();
+| void halt(void);
 .global halt
 halt:
 	stop	#0x2700
 	rts
+
+| NB: cpuidle() works on both the TI-89 and TI-92+, but it doesn't power off
+| the FlashROM on the 92+
+| void cpuidle(void);
+.global cpuidle
+cpuidle:
+	move.w	%sr,%d1
+	move.w	#0x2700,%sr
+	move.w	#0x280,0x600018
+	moveq	#0x1f,%d0
+	move.b	%d0,0x600005 | shut off the cpu until an interrupt is triggered
+	nop
+	move.w	#0x2000,%sr  | let interrupt handlers run
+	move.w	%d1,%sr
+	rts
+
 
 | void delay(unsigned long n);
 .global delay
