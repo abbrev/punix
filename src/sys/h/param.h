94299c535367cb16502399fddd34546ee2843cda
Christopher Williams <abbrev@gmail.com>
2010-03-29 03:04:24 -0700

    remove old/unneeded parameters

--- src/sys/h/param.h
+++ src/sys/h/param.h
@@ -14,26 +14,21 @@
  * tunable variables
  */
 
-#define	NBUF	29		/* size of buffer cache */
 #define	MINBUF	8		/* minimum number of buffers */
-#define	NINODE	200		/* number of in core inodes */
-#define	NFILE	175		/* number of in core file structures */
+#define MAXBUF  32
 #define	NMOUNT	8		/* number of mountable file systems */
 #define	MAXMEM	(64*32)		/* max core per process - first # is Kw */
 #define	MAXUPRC	25		/* max processes per user */
 #define	NOFILE	32		/* max open files per process */
 #define	CANBSIZ	QSIZE		/* max size of typewriter line */
-#define	CMAPSIZ	50		/* size of core allocation area */
-#define	SMAPSIZ	50		/* size of swap allocation area */
 #define	NCALL	20		/* max simultaneous time callouts */
-#define	NPROC	64		/* max number of processes */
 #define	NGROUPS	16		/* max number of groups */
 #define	NOGROUP	((gid_t)-1)		
 #define	NTEXT	40		/* max number of pure texts */
-#define	NCLIST	100		/* max total clist size */
 #define	HZ	256		/* clock rate (ticks/second) */
 #define	SECOND  1000000000L	/* nanoseconds in a second */
 #define TICK	(SECOND / HZ) /* nanoseconds/tick of the clock */
+
 #define FLASH_CACHE_SIZE 32
 #define HEAPSIZE 512
 #define HEAPBLOCKSIZE 16
@@ -91,24 +86,16 @@
 
 #define	NBPW	sizeof(int)	/* number of bytes in an integer */
 #define	BSIZE	512		/* size of secondary block (bytes) */
-/* BSLOP can be 0 unless you have a TIU/Spider */
-#define	BSLOP	2		/* In case some device needs bigger buffers */
-#define	NINDIR	(BSIZE/sizeof(daddr_t))
 #define	BMASK	0777		/* BSIZE-1 */
 #define	BSHIFT	9		/* LOG2(BSIZE) */
-#define	NMASK	0177		/* NINDIR-1 */
-#define	NSHIFT	7		/* LOG2(NINDIR) */
 #define	USIZE	16		/* size of user block (*64) */
-#define	UBASE	0140000		/* abs. addr of user block */
 #define	CMASK	0		/* default mask for file creation */
 #define	NODEV	(dev_t)(-1)
-#define	ROOTINO	((ino_t)2)	/* i number of all roots */
-#define	SUPERB	1		/* block number of the super block */
-#define	NICINOD	100		/* number of superblock inodes */
-#define	NICFREE	50		/* number of superblock free blocks */
-#define	INFSIZE	138		/* size of per-proc info for users */
-#define	CBSIZE	14		/* number of chars in a clist block */
-#define	CROUND	017		/* clist rounding: sizeof(int *) + CBSIZE - 1*/
+//#define	ROOTINO	((ino_t)2)	/* i number of all roots */
+//#define	SUPERB	1		/* block number of the super block */
+//#define	NICINOD	100		/* number of superblock inodes */
+//#define	NICFREE	50		/* number of superblock free blocks */
+//#define	INFSIZE	138		/* size of per-proc info for users */
 
 #if 0 /* make this work in Punix on m68k */
 /*
@@ -156,11 +143,6 @@ typedef char *		caddr_t;
  */
 #define	UMODE	0x2000		/* usermode bits */
 #define	USERMODE(ps)	(((ps) & UMODE)==0)
-
-#if 0
-#define	INTPRI	0340		/* Priority bits */
-#define	BASEPRI(ps)	((ps & INTPRI) != 0)
-#endif
 
 #define DEV_BSIZE  128
 #define DEV_BSHIFT 7
