93a996ec8df422f4fbde3df4c0a6762d5fd773ce
Christopher Williams <abbrev@gmail.com>
2010-04-15 16:04:19 -0700

    remove redundant implementation of ffs()

--- src/sys/sys/ffs.c
+++ src/sys/sys/ffs.c
@@ -18,15 +18,5 @@ int ffsl(long n)
 
 int ffs(int n)
 {
-	static const char table[] = {
-		33, 1, 26, 2, 23, 27, 32, 3,
-		16, 24, 30, 28, 11, 33, 13, 4,
-		7, 17, 33, 25, 22, 31, 15, 29,
-		10, 12, 6, 33, 21, 14, 9, 5,
-		20, 8, 19, 18
-	};
-	if (n == 0)
-		return 0;
-	int m = (unsigned)(n ^ (n - 1)) % 37;
-	return table[m];
+	return ffsl(n);
 }
