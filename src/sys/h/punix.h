afeb6bbd246588627dff8439b8152711a14d91f3
Christopher Williams <abbrev@gmail.com>
2011-03-02 01:07:27 -0700

    converted some common routines to inline assembly

--- src/sys/h/punix.h
+++ src/sys/h/punix.h
@@ -66,10 +66,34 @@ struct syscallframe {
 /* useful macro for iterating through each process */
 //#define EACHPROC(p)	((p) = G.proclist; (p); (p) = (p)->p_next)
 
-int spl0(void), spl1(void), spl2(void), spl3(void);
-int spl4(void), spl5(void), spl6(void), spl7(void);
-int splx(int);
-int splclock(void);
+static inline void nop() { asm volatile ("nop"); }
+static inline void halt() { asm volatile ("stop #0x2700"); }
+static inline void splx(int x)
+{
+	asm volatile ("move %0,%%d0; move %%d0,%%sr"
+	              : /* no output */
+	              : "gr"(x)
+	              : "d0");
+}
+static inline int spl(int x)
+{
+	int ret;
+	asm volatile ("move %%sr,%%d0; move %%d0,%0 ; move %1*256+0x2000,%%sr"
+	              : "=r"(ret)
+	              : "i"(x)
+	              : "d0");
+	return ret;
+}
+#define spl0() spl(0)
+#define spl1() spl(1)
+#define spl2() spl(2)
+#define spl3() spl(3)
+#define spl4() spl(4)
+#define spl5() spl(5)
+#define spl6() spl(6)
+#define spl7() spl(7)
+#define splclock() spl3()
+
 void stop(struct proc *);
 
 void panic(const char *s);
