b7f7843f46877bfb740be245b007ea1feec6d8aa
Christopher Williams <abbrev@gmail.com>
2011-03-02 01:09:59 -0700

    moved some routines to inline assembly code in punix.h

--- src/sys/sys/m68k.s
+++ src/sys/sys/m68k.s
@@ -1,43 +1,5 @@
 .section _st1, "x"
 
-/* set the interrupt level to 'x' and return the old one */
-.global splx
-| void splx(int x);
-splx:
-	move	%sr,%d0
-	move	4(%sp),%sr
-	rts
-
-| int spl'n'();
-.macro	defspl	level
-.global spl\level
-spl\level:
-	move	%sr,%d0 | save the old %sr
-	move	#0x2000+(\level<<8),%sr
-	rts
-.endm
-
-| XXX: not all of these levels are used
-defspl	0 | 
-defspl	1 | 256Hz timer
-|defspl	2 | key press (never used)
-.global splclock
-splclock:
-defspl	3 | time
-defspl	4 | link port
-defspl	5 | audio (link port)
-|defspl	6 | on key
-defspl	7 | block everything (except memory write error)
-
-.global nop
-nop:	rts
-
-| void halt(void);
-.global halt
-halt:
-	stop	#0x2700
-	rts
-
 | NB: cpuidle() works on both the TI-89 and TI-92+, but it doesn't power off
 | the FlashROM on the 92+
 | void cpuidle(void);
