7736880116fc93fa62a54aac65019c9442264bb4
Christopher Williams <abbrev@gmail.com>
2011-03-02 19:55:24 -0700

    add backtrace and stacktrace routines

--- src/sys/sys/m68k.s
+++ src/sys/sys/m68k.s
@@ -23,3 +23,61 @@ delay:
 0:	subq.l	#1,%d0
 	bne.s	0b
 	rts
+
+| int backtrace(void **buffer, int size);
+.global backtrace
+backtrace:
+	move.l	4(%sp),%a0
+	move	8(%sp),%d0
+	
+	move.l	%fp,%a1
+	move.l	%fp,%d2
+	bra	1f
+	
+0:
+		move.l	%a1,%d1
+		beq	2f	| it's zero
+		btst	#0,%d1
+		bne	2f	| not a valid pointer
+		cmp.l	#0x40000,%d1	| upper limit of RAM
+		bhi	2f
+		|cmp.l	%d2,%d1
+		|blo	2f	| we went in the opposite direction
+		move.l	%d1,(%a0)+	| put it in the buffer
+		
+		move.l	4(%a1),%d2
+		move.l	(%a1),%a1	| get the next frame
+1:		dbra	%d0,0b
+2:	sub.l	4(%sp),%a0
+	move.l	%a0,%d0
+	lsr.l	#2,%d0
+	rts
+
+| int stacktrace(void **buffer, int size);
+.global stacktrace
+stacktrace:
+	move.l	4(%sp),%a0
+	move	8(%sp),%d0
+	
+	move.l	%fp,%a1
+	move.l	%fp,%d2
+	bra	1f
+	
+0:
+		move.l	%a1,%d1
+		beq	2f	| it's zero
+		btst	#0,%d1
+		bne	2f	| not a valid pointer
+		cmp.l	#0x40000,%d1	| upper limit of RAM
+		bhi	2f
+		|cmp.l	%d2,%d1
+		|blo	2f	| we went in the opposite direction
+		move.l	%d1,(%a0)+	| put it in the buffer
+		
+		move.l	%a1,%d2
+		move.l	(%a1),%a1	| get the next frame
+1:		dbra	%d0,0b
+2:	sub.l	4(%sp),%a0
+	move.l	%a0,%d0
+	lsr.l	#2,%d0
+	rts
