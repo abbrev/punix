c555dc870b7c75f48d4ef1d0e4dfe16b87c67034
Christopher Williams <abbrev@gmail.com>
2011-03-02 20:12:03 -0700

    add stacktrace to panic message

--- src/sys/sys/panic.c
+++ src/sys/sys/panic.c
@@ -1,10 +1,18 @@
+#include <execinfo.h>
+
 #include "punix.h"
 
 int kputs(const char *s);
 
 STARTUP(void panic(const char *s))
 {
-	kprintf("\nkernel panic: %s", s);
+	void *bt[18];
+	int i, n;
+	kprintf("\nkernel panic: %s    \n", s);
+	n = stacktrace(bt, 18);
+	kprintf("stacktrace:    \n", n);
+	for (i = 0; i < n; ++i)
+		kprintf("%06lx    \n", bt[i]);
 	while (1)
 		nop();
 }
