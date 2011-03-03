c2493d42c4506a555a8aebf05501b81c77523fac
Christopher Williams <abbrev@gmail.com>
2011-03-03 13:38:07 -0700

    make shell ignore empty lines

--- src/sys/sys/usermain.c
+++ src/sys/sys/usermain.c
@@ -1042,13 +1042,6 @@ static void showhelp()
 	printf("\n");
 }
 
-enum {
-	TOKEN_ERR_SUCCESS = 0,
-	TOKEN_ERR_EOL,
-	TOKEN_ERR_UNMATCHED_DQUOTE,
-	TOKEN_ERR_UNMATCHED_SQUOTE
-};
-
 const unsigned char _ctype[256] = {
 	_C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C,
 	_C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C, _C,
@@ -1068,26 +1061,12 @@ const unsigned char _ctype[256] = {
 	 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
 };
 
-/*
-          2 3 4 5 6 7       30 40 50 60 70 80 90 100 110 120
-        -------------      ---------------------------------
-       0:   0 @ P ` p     0:    (  2  <  F  P  Z  d   n   x
-       1: ! 1 A Q a q     1:    )  3  =  G  Q  [  e   o   y
-       2: " 2 B R b r     2:    *  4  >  H  R  \  f   p   z
-       3: # 3 C S c s     3: !  +  5  ?  I  S  ]  g   q   {
-       4: $ 4 D T d t     4: "  ,  6  @  J  T  ^  h   r   |
-       5: % 5 E U e u     5: #  -  7  A  K  U  _  i   s   }
-       6: & 6 F V f v     6: $  .  8  B  L  V  `  j   t   ~
-       7: ´ 7 G W g w     7: %  /  9  C  M  W  a  k   u  DEL
-       8: ( 8 H X h x     8: &  0  :  D  N  X  b  l   v
-       9: ) 9 I Y i y     9: ´  1  ;  E  O  Y  c  m   w
-       A: * : J Z j z
-       B: + ; K [ k {
-       C: , < L \ l |
-       D: - = M ] m }
-       E: . > N ^ n ~
-       F: / ? O _ o DEL
-*/
+enum {
+	TOKEN_ERR_SUCCESS = 0,
+	TOKEN_ERR_EOL,
+	TOKEN_ERR_UNMATCHED_DQUOTE,
+	TOKEN_ERR_UNMATCHED_SQUOTE
+};
 
 /*
  * get a token from the string *s, null terminate the token in *s, update *s
@@ -1228,10 +1207,11 @@ static int sh_main(int argc, char **argv, char **envp)
 					break;
 				}
 				printf("stupidsh: error: %s\n", errstr);
-				goto nextline;
+				goto eol;
 			}
 			aargv[aargc] = NULL;
 			
+			if (aargc == 0) goto nextline;
 			cmd = aargv[0];
 			if (!strcmp(cmd, "exit")) {
 				break;
@@ -1245,10 +1225,11 @@ static int sh_main(int argc, char **argv, char **envp)
 					   cmd);
 				}
 			}
+eol:
+			neof = 0;
 nextline:
 			bp = buf;
 			len = 0;
-			neof = 0;
 		}
 	}
 	return 0;
