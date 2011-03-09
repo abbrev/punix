4ac56a496496a7be91efad85e79bc89fb33ae0e3
Christopher Williams <abbrev@gmail.com>
2011-03-09 00:03:41 -0700

    remove debugging code

--- src/sys/sys/process.c
+++ src/sys/sys/process.c
@@ -178,17 +178,6 @@ STARTUP(void wakeup(void *chan))
 	}
 }
 
-void print_list(struct list_head *lp, char *s)
-{
-	struct list_head *listp;
-
-	list_for_each(listp, lp) {
-		kprintf(" %08lx next=%08lx prev=%08lx (%s)\n",
-			listp, listp->next, listp->prev, s);
-	}
-}
-
-
 /* allocate a process structure */
 STARTUP(struct proc *palloc())
 {
@@ -214,7 +203,6 @@ STARTUP(int pidalloc())
 	static int mpid = 1;
 	*/
 	
-	//print_list(&G.proc_list, "pidalloc: proc list");
 	/*
 	 * mpid is the current pid.
 	 * pidchecked is the lowest pid after mpid that is currently used by a
@@ -223,7 +211,6 @@ STARTUP(int pidalloc())
 	 */
 	++G.mpid;
 retry:
-	//kprintf("%s (%d)\n", __FILE__, __LINE__);
 	if (G.mpid >= MAXPID) {
 		G.mpid = 2;
 		G.pidchecked = 0;
