987a1ec46e8e64a3cfc6a0f07171177e448a0be6
Christopher Williams <abbrev@gmail.com>
2011-06-30 22:28:07 -0700

    auto-generate sysent.c file from the master list, sysent.txt

--- src/sys/h/sysent.h
+++ src/sys/h/sysent.h
@@ -1,8 +1,8 @@
-extern struct sysent {
+extern const struct sysent {
 	int	sy_narg; /* number of _words_ of args */
 	void	(*sy_call)();
 	int	sy_flags;
 } sysent[];
 
 extern const int nsysent;
-extern const char *sysname[];
+extern const char *const sysname[];
