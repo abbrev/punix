33b9adcfeb0065cfbe75f89d8dc7998bddf4e752
Christopher Williams <abbrev@gmail.com>
2011-08-03 21:27:03 -0700

    display backtrace instead of stacktrace

--- src/sys/sys/panic.c
+++ src/sys/sys/panic.c
@@ -6,11 +6,12 @@ int kputs(const char *s);
 
 STARTUP(void panic(const char *s))
 {
-	void *bt[18];
+#define BACKTRACE_BUF_SIZE 20
+	void *bt[BACKTRACE_BUF_SIZE];
 	int i, n;
 	kprintf("\nkernel panic: %s    \n", s);
-	n = stacktrace(bt, 18);
-	kprintf("stacktrace:    \n", n);
+	n = backtrace(bt, BACKTRACE_BUF_SIZE);
+	kprintf("backtrace:    \n", n);
 	for (i = 0; i < n; ++i)
 		kprintf("%06lx    \n", bt[i]);
 	while (1)
