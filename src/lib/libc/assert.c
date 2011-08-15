8e3daa7fec78eb3fc875548e336113341736d30d
Christopher Williams <abbrev@gmail.com>
2011-08-15 13:18:41 -0700

    remove kernel-specific stuff

--- src/lib/libc/assert.c
+++ src/lib/libc/assert.c
@@ -1,10 +1,9 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#include "punix.h"
-
-STARTUP(void __assert(const char *expr, const char *file, unsigned line))
+void __assert(const char *expr, const char *file, unsigned line)
 {
-	kprintf("%s:%u: Assertion `%s' failed.\n", file, line, expr);
+	fprintf(stderr, "%s:%u: Assertion `%s' failed.\n", file, line, expr);
 	abort();
 }
